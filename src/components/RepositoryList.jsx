5912536082f449d6583fe00de074a552e52dda9a
Jonathas Bonfim <jonathas@atualsistemas.com.br>
2022-03-24 16:55:16 -0300

    useState and useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,9 @@
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
+import { useState, useEffect } from "react/cjs/react.production.min";
+
+// https://api.github.com/users/jonathas-bonfim
 
 const repository = {
   name: "repository",
@@ -9,6 +12,7 @@ const repository = {
 }
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
   return (
     <section className="repository-list">
       <h1>Lista de Repositórios</h1>
